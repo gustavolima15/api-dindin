f3910160d969e098955790c2a90ce0a020711423
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-06-14 00:13:27 -0300

    feat: auth

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,9 +1,10 @@
 const pool = require('../conexao');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const senhaJwt = require('../configs');
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
-    console.log(nome, email, senha);
     try {
         const senhaCriptografada = await bcrypt.hash(senha, 10)
         const { rows } = await pool.query(
@@ -27,8 +28,33 @@ const listarUsuarios = async (req, res) => {
 	}
 }
 
+const login = async (req, res) => {
+    const {email, senha} = req.body;    
+    try {        
+        const usuario = await pool.query('select * from usuarios where email = $1', [email])
+        
+        if(usuario.rowCount < 1){
+            return res.status(404).json({mensagem: 'Usuário não encontrado'})
+        }
+        
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha);
+        if (!senhaValida) {
+            return res.status(400).json({mensagem: 'Senha e/ou email incorretos'})
+        }
+
+        const token = jwt.sign({id: usuario.rows[0].id}, senhaJwt.jwtSecret, {expiresIn: '8h'})
+        const { senha: _, ...usuarioLogado } = usuario.rows[0]
+
+        return res.status(201).json({usuario: usuarioLogado, token}) 
+    } catch (error) {
+            return res.status(500).json({mensagem: 'Erro interno do servidor'})
+    }
+
+}
+
 
 module.exports = {
     cadastrarUsuario,
-    listarUsuarios
+    listarUsuarios,
+    login
 };
